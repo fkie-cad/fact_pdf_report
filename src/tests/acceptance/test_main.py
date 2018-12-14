1bd1b06edb6a826008aa1dd58fdeaba8dc9eddb5
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2018-12-14 15:18:24 +0100

    tests added

--- src/tests/acceptance/test_main.py
+++ src/tests/acceptance/test_main.py
@@ -1,12 +1,17 @@
 from common_helper_process.fail_safe_subprocess import execute_shell_command_get_return_code
 import os
+import pytest
 
 
 SRC_DIR = os.path.dirname(os.path.abspath(__file__)) + '/../../pdf_generator.py'
 
 
-def test_main_program():
-    command_line = SRC_DIR + ' -V'
+@pytest.mark.parametrize('arguments, expected_output, expected_return_code', [
+    ('-V', 'FACT', 0),
+    ('-h', 'usag', 0)
+])
+def test_main_program(arguments, expected_output, expected_return_code):
+    command_line = SRC_DIR + ' ' + arguments
     output, return_code = execute_shell_command_get_return_code(command_line)
-    print(output)
-    assert return_code == 0
+    assert output[0:4] == expected_output
+    assert return_code == expected_return_code
